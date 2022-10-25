57728c8201ebda31f47ff78fdacdb499a9b72150
CrazySneeze <matthew.k00@outlook.com>
2022-10-25 20:26:02 +0100

    Fixed issue with bad texturing and missing rigid body

--- survival game/Assets/Scripts/Player/MoveControl.cs
+++ survival game/Assets/Scripts/Player/MoveControl.cs
@@ -1,7 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-//using Unity.Burst.CompilerServices;
 using UnityEngine;
 
 public class MoveControl : MonoBehaviour
