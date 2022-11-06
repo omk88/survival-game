43f563f3b1b66299b1379dd4d87bde1110042e4e
DeaglanOC <78208543+DeaglanOC@users.noreply.github.com>
2022-11-06 15:22:27 +0000

    UI (healthbar and inventory continue)
    moved items to prefab folder , added a health bar and UI elements for inventory system

---
+++ survival game/Assets/healthBar.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class healthBar : MonoBehaviour //based on tutorial-add link
+{
+    public Image FillImage;
+    public float CurrentHealth;
+
+    public void UpdateHP()
+    {
+        FillImage.fillAmount = CurrentHealth / 100;
+
+    }
+}
