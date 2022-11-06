1ec2cc926ccc27af6ad3235868e0021e9b522a12
DeaglanOC <78208543+DeaglanOC@users.noreply.github.com>
2022-11-06 21:30:14 +0000

    Inventory 3

---
+++ survival game/Assets/Scripts/UI/InventoryManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryManager : MonoBehaviour //based on online tutorial-add link
+{
+    public static InventoryManager Instance;
+    public List<SItem> Items = new List<SItem>();
+
+    private void Awake()
+    {
+        Instance=this;
+    }
+
+    public void Add (SItem item)
+    {
+        Items.Add(item);
+    }
+
+    public void Remove(SItem item)
+    {
+        Items.Remove(item);
+    }
+
+}
---
+++ survival game/Assets/Scripts/UI/ItemController.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemController : MonoBehaviour // based on GUI video tutorial-add link
+{
+    public SItem Item;
+}
