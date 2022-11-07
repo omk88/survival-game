9da23590a4c71825c7394b9b865caeed7b0dd965
DeaglanOC <78208543+DeaglanOC@users.noreply.github.com>
2022-11-07 19:56:18 +0000

    respawn base
    scripts/Player/death to be used as base for respawn

---
+++ survival game/Assets/Scripts/Player/Death.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Death : MonoBehaviour
+{
+    public int CurrentHealth;
+    private PlayerManager playerManager;
+    public int ReScene;
+
+    //CurrentHealth=GameObject.FindGameObjectWithTag("playerManager").GetComponent<PlayerManager>().player.health;
+
+    void Awake()
+    {
+        playerManager = GetComponent<PlayerManager>();
+    }
+
+    private void Start()
+    {
+        Debug.Log(playerManager.player.health);
+    }
+
+    public void respawn()
+    {
+        if (CurrentHealth < 1)
+        {
+            //SceneManager.LoadScene(SceneManager.ActiveScene().buildIndex);//rebuilds current scene, both this and the below are differnt ways to restart the scene
+            //SceneManager.LoadScene(ReScene);//rescene would be set to the scene index of the current scene(playtesting is currently 0 if you check build settings
+        }
+
+    }
+}
---
+++ survival game/Assets/Scripts/Spawner.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+
+    public GameObject enemy;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Instantiate(enemy);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
